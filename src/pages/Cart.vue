bfc593fc76ab5978c5169ac5c87bff744686fa0c
svskpt1998 <svspkt1998@gmail.com>
2021-10-12 15:24:56 -0700

    viet hoa header mobile

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -3,7 +3,7 @@
     <div class="d-none">
       <div class="bg-primary border-bottom p-3 d-flex align-items-center">
         <a class="toggle togglew toggle-2" href="#"><span></span></a>
-        <h4 class="font-weight-bold m-0 text-white">Checkout</h4>
+        <h4 class="font-weight-bold m-0 text-white">Thanh toán</h4>
       </div>
     </div>
     <!-- checkout -->
--- src/pages/CustomerOrder.vue
+++ src/pages/CustomerOrder.vue
@@ -2,7 +2,7 @@
   <div class="d-none">
     <div class="bg-primary border-bottom p-3 d-flex align-items-center">
       <a class="toggle togglew toggle-2" href="#"><span></span></a>
-      <h4 class="font-weight-bold m-0 text-white">My Order</h4>
+      <h4 class="font-weight-bold m-0 text-white">Đơn hàng</h4>
     </div>
   </div>
   <section class="py-4 osahan-main-body">
