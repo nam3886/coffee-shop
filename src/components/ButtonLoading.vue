06b3317a66eab30f6a8de53a942a4d3af887478f
svskpt1998 <svspkt1998@gmail.com>
2021-10-11 20:21:37 -0700

    loading button

---
+++ src/components/ButtonLoading.vue
@@ -0,0 +1,11 @@
+<template>
+  <button class="buttonload" @click="clickEvent">
+    <i class="fa fa-spinner fa-spin"></i>Loading...
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
